311bf93c041ce1539c0258c08f65b72e4435450f
samuelestrellalopez <22091052@alumno.utmetropolitana.edu.mx>
2025-03-18 20:19:15 -0600

    changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
+<<<<<<< HEAD
 import LandingView from '@/views/Landing.vue'; // Asegúrate de que el nombre del archivo sea correcto
 import LoginView from '@/views/Login.vue';
 import UserView from '@/views/UserView.vue'; // Importa la nueva vista del usuario
 import EventDetail from '@/components/Events/EventDetail.vue';
 import HomeView from '../views/HomeView.vue';
+=======
+import LandingView from '@/views/Landing.vue';
+import LoginView from '@/views/Login.vue';
+import UserView from '@/views/UserView.vue';
+import EventDetail from '@/components/Events/EventDetail.vue';
+import HomeView from '../views/HomeView.vue';
+import EventPage from'@/views/PageEvent.vue';
+>>>>>>> 6f75e95 (vistas nuevas)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,7 +20,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'landing',
+<<<<<<< HEAD
       component: LandingView, // Landing será la vista por defecto
+=======
+      component: LandingView, 
+>>>>>>> 6f75e95 (vistas nuevas)
     },
     {
       path: '/home',
@@ -27,6 +40,7 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+<<<<<<< HEAD
     },
     {
       path: '/user',
@@ -37,7 +51,25 @@ const router = createRouter({
       path: '/event-detail',
       name: 'event-detail',
       component: EventDetail,
+=======
+>>>>>>> 6f75e95 (vistas nuevas)
     },
+    {
+      path: '/user',
+      name: 'user',
+      component: UserView, 
+    },
+    {
+      path: '/event-detail',
+      name: 'event-detail',
+      component: EventDetail,
+    },
+    {
+      path: '/event-page',
+      name: 'event-page',
+      component: EventPage,
+
+    }
   ],
 });
 
