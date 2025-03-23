0b06fff51a6bad760385a330469cb0e837d7278e
DanielCocom <danielalexdcd@gmail.com>
2025-03-23 00:26:15 -0600

    Se crea modelo y factory de owner

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Category;
+use App\Models\Owner;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
         // me dio weva hacerlo en una clase
         User::factory()->count(10)->create();
         Category::factory()->count(10)->create();
+        Owner::factory()->count(10)->create();
         $this->call([
             VenueSeeder::class,
             EventSeeder::class,
