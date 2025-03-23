e50967168e1bb4f29dc99e0343f1287d0f621ed5
DanielCocom <danielalexdcd@gmail.com>
2025-03-23 00:23:18 -0600

    Se modifican migraciones
    Se modificaron las migraciones de events y se crea la tabla owner

--- backend/database/migrations/2025_03_19_000249_create_events_table.php
+++ backend/database/migrations/2025_03_19_000249_create_events_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
+            $table->text('image_url');
             $table->dateTime('start_date');
             $table->dateTime('end_date');
             $table->foreignId('category_id')->constrained();
