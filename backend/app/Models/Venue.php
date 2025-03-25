e3a7d0e650d3d5a9ea4913e1c4fb6585a5241c8f
DanielCocom <danielalexdcd@gmail.com>
2025-03-25 12:00:41 -0600

    Se modifica modelo venue

--- backend/app/Models/Venue.php
+++ backend/app/Models/Venue.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Venue extends Model
 {
     //
     use HasFactory;
+
+    public function getMaxRegistrations($eventId)
+    {
+        return DB::table('registrations')
+            ->where('event_id', $eventId)
+            ->max('event_id');
+    }
 }
