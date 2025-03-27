4e7d16f9246240b7a6a3fd08d77bd9bbf94ad861
MartinFlakes <martin.rayer19@gmail.com>
2025-03-26 18:56:48 -0600

    Se agrega middleware en metodos necesarios

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,13 +8,12 @@ use App\Http\Controllers\AuthController;
 // Rutas de autenticación
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
 Route::post('/refresh', [AuthController::class, 'refresh']);
-Route::post('/me', [AuthController::class, 'me']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+    
+});
 
 Route::get('/test', function () {
     return response()->json(['message' => '¡Hola desde Laravel!']);
